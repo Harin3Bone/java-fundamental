985413617b38b6b21251c1f261c638a179a74b7a
Harin3Bone <harin.thananam@gmail.com>
2022-02-05 14:21:51 +0700

    Flow - update challenge for loop (isPrime)

--- 4-Flow/src/ForStatement.java
+++ 4-Flow/src/ForStatement.java
@@ -29,9 +29,57 @@ public class ForStatement {
 			double interest = calculateInterest(initValue, rate);
 			System.out.println(initValue + " at " + rate + "% interest = " + String.format("%.2f", interest));
 		}
+
+		/*
+		 * Challenge Create a for statement using any range of numbers Determine if the
+		 * number is a prime number using the isPrime method if it is a prime number,
+		 * print it out AND increment a count of the number of prime numbers found if
+		 * that count is 3 exit the for loop hint: Use the break; statement to exit
+		 */
+
+		int count = 0;
+		for (int no = 0; no < 20; no++) {
+			if (count == 3) {
+				break;
+			}
+
+			if (isPrime(no)) {
+				System.out.println("Number " + no + " is prime number.");
+				count++;
+			}
+		}
+
 	}
 
 	public static double calculateInterest(double amount, double interestRate) {
 		return (amount * (interestRate / 100));
+	}
+
+	public static boolean isPrime(int n) {
+		if (n == 0 || n == 1) {
+			return false;
+		}
+
+		for (int i = 2; i <= n / 2; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	
+	public static boolean isPrimeV2(int n) {
+		if (n == 0 || n == 1) {
+			return false;
+		}
+
+		for (int i = 2; i <= (long) Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
